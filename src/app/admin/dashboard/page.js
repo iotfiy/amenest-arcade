849683d3c7c86beb66c25fe4ea4dedca24261491
Muhammad Umar Waqar <mohammthegreat@gmail.com>
2024-11-30 01:02:41 +0500

    Init Commit

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -77,9 +77,9 @@ function Page() {
           {
             name: "Sales",
             data: [
-              { x: "Apr", y: 50, fillColor: "#6a0dad" },  // Purple
-              { x: "May", y: 40, fillColor: "#8a2be2" },  // Blue-violet
-              { x: "Jun", y: 300, fillColor: "#7b68ee" },  // Medium slate blue
+              { x: "Apr", y: 150, fillColor: "#6a0dad" },  // Purple
+              { x: "May", y: 240, fillColor: "#8a2be2" },  // Blue-violet
+              { x: "Jun", y: 500, fillColor: "#7b68ee" },  // Medium slate blue
               { x: "Jul", y: 320, fillColor: "#4169e1" },  // Royal blue
               { x: "Aug", y: 500, fillColor: "#0000ff" },  // Blue
               { x: "Sep", y: 350, fillColor: "#4682b4" },  // Steel blue
@@ -88,11 +88,11 @@ function Page() {
         ],
         chart: {
           type: "bar",
-          height: 400,
+          height: 300,
           toolbar: {
             show: false,
           },
-          borderRadius: 10, // Optional: Add rounded corners to the chart container
+         
           stroke: {
             show: true,
             width: 10, // Border thickness
@@ -146,12 +146,14 @@ function Page() {
           },
         },
         grid: {
-          show: true,
-          borderColor: "#dddddd",
-          strokeDashArray: 5,
-          xaxis: {
+          yaxis:{
             lines: {
               show: true,
+            }
+          },
+          xaxis: {
+            lines: {
+              show: false,
             },
           },
           padding: {
@@ -183,10 +185,10 @@ function Page() {
 
   return (
     <div>
-      <div className='grid grid-cols-2 gap-x-5   mt-4'>
+      <div className='grid xl:grid-cols-2 grid-cols-1 gap-x-5   mt-4'>
         <div className='flex flex-col min-w-[850px] justify-center items-center'>
           <div className='bg-custom-headpurple flex rounded-full items-center justify-between p-2 w-[800px]'>
-            <h1 className='ml-3'>Dashboard</h1>
+            <h1 className='ml-3 font-bold text-xl'>Dashboard</h1>
             <div className='flex'>
               <Image src="/dash1.svg" height={100} width={100} />
               <Image src="/dash2.svg" height={100} width={100} />
@@ -195,9 +197,9 @@ function Page() {
           </div>
 
           {/* Cards */}
-          <div className="grid grid-cols-3 my-10 pt-10 ml-[5px]  gap-y-20  max-h-[500px] overflow-y-auto ">
+          <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 my-10 pt-10 ml-[5px]  gap-y-20  max-h-[500px] overflow-y-auto ">
             {arcades.map((arcade) => (
-              <div key={arcade.id} className="bg-white border border-black shadow-md rounded-xl w-[250px] mx-5 relative p-5 flex flex-col items-center justify-center gap-y-3 ">
+              <div key={arcade.id} className="  bg-white border border-black shadow-md rounded-xl w-[250px] mx-5 relative p-5 flex flex-col items-center justify-center gap-y-3 ">
                 <div className="z-[-1] bg-custom-headpurple px-3 flex items-center justify-around absolute top-[-35px] left-0 rounded-t-xl py-3">
                   <h1 className="font-mono text-sm">SH-KHI-A001</h1>
                   <div className="text-sm ml-3">ARCADE ID</div>
@@ -226,14 +228,14 @@ function Page() {
 
 
         </div>
-        <div className='ml-[200px]'>
+        <div className='ml-[200px] mt-5'>
           <div>
             <div>
               <DashVenues />
             </div>
 
-            <div className='bg-white border border-black rounded-xl p-3 '>
-              <div className='grid grid-cols-3 p-2 gap-x-2'>
+            <div className='bg-white border border-black rounded-xl p-3 m-2 md:w-[450px] xl:w-[400px] '>
+              <div className='grid grid-cols-3 p-2 gap-x-2 '>
                 <div className='border border-gray-300 rounded-3xl shadow-md   flex flex-col items-center justify-center py-4 gap-y-3'>
                   <h1 className="text-xl">Hard Play</h1>
                   <div className='flex items-center '>
@@ -263,15 +265,14 @@ function Page() {
               
 
               {/* Chart */}
-              <div className="dashboard-graph max-w-[400px] ">
-                <div className="relative flex flex-col bg-white bg-clip-border text-gray-700 shadow-md" style={{ height: "450px" }}>
+              <div className="  mx-auto  dashboard-graph max-w-[400px] max-h-[350px] border border-black overflow-y-hidden">
+                <div className="relative flex flex-col bg-white bg-clip-border text-gray-700 shadow-md" style={{ height: "400px" }}>
 
                   <div className="pt-6 px-2 pb-0">
                     <div id="bar-chart"></div>
                   </div>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
