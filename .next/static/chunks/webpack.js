00afbff795fdf2535acaea59585070bee0e66468
Umer Farooq <aaafarooq285@gmail.com>
2024-12-01 01:30:48 +0500

    some changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "00ffe82b75c0a75a"; }
+/******/ 		__webpack_require__.h = function() { return "9eed5a1ff46d4ab0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
